bf5ddb4593c7ba1cb8dc8b441a38c9fa65082397
spekary <spekary@gmail.com>
2022-03-22 11:05:56 -0700

    Breaking out some useful interfaces

--- mapi.go
+++ mapi.go
@@ -2,16 +2,31 @@ package maps
 
 // MapI is the interface used by all the Map types.
 type MapI[K comparable, V any] interface {
+	Setter[K, V]
+	Getter[K, V]
+	Loader[K, V]
 	Clear()
 	Len() int
 	Range(func(k K, v V) bool)
-	Load(k K) (v V, ok bool)
-	Get(k K) (v V)
 	Has(k K) bool
 	Keys() []K
 	Values() []V
-	Set(K, V)
 	Merge(MapI[K, V])
 	Equal(MapI[K, V]) bool
 	Delete(k K)
+}
+
+// Setter sets a value in a map.
+type Setter[K comparable, V any] interface {
+	Set(K, V)
+}
+
+// Getter gets a value from a map.
+type Getter[K comparable, V any] interface {
+	Get(k K) (v V)
+}
+
+// Loader loads a value from a map.
+type Loader[K comparable, V any] interface {
+	Load(k K) (v V, ok bool)
 }
